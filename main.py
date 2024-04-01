f9c94b6bf7ccc5fea8c0b235b7f8c95bb96be4c2
RohanS. <rohan23443@iiitd.ac.in>
2024-04-01 21:50:47 +0530

    wasd working plus game over screen working

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from game import Game
-import sys
+from sys import exit
+from itertools import cycle
 pg.init()
 
 screen = pg.display.set_mode((300,600))
@@ -8,36 +9,50 @@ pg.display.set_caption('hello')
 clock = pg.time.Clock()
 game = Game()
 GAME_UPDATE = pg.USEREVENT
-pg.time.set_timer(GAME_UPDATE,20)
-game_over_text = pg.font.Font(None,size = 40)
-text_surf = game_over_text.render(
-'''Press Any Key 
-   to Restart''', False, 'White')
-text_rect = text_surf.get_rect(center = (150,150))
-while True:
+pg.time.set_timer(GAME_UPDATE,300)
 
+#game over text
+GAME_OVER = pg.USEREVENT+1
+pg.time.set_timer(GAME_OVER,600)
+game_over_text = pg.font.Font('Minecraft.ttf',size = 35)
+text_surf = game_over_text.render(
+''' Press Any Key 
+   to Restart''', True, 'White')
+text_rect = text_surf.get_rect(center = (150,200))
+no_text_surf = pg.surface.Surface(text_rect.size)
+no_text_surf.set_colorkey((0,0,0))
+text_blink_surf = cycle([text_surf,no_text_surf])
+t_b_s = next(text_blink_surf)
+
+
+while True:
+    screen.fill((44,44,127))
+    game.draw(screen)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
-            sys.exit()
-        if event.type == pg.KEYDOWN:
+            exit()
+        if event.type == pg.KEYDOWN or event.type == pg.MOUSEBUTTONDOWN:
             if game.game_over:
                 game.reset()
                 
-            if event.key == pg.K_LEFT and not game.game_over:
+        if event.type == pg.KEYDOWN:
+            if event.key in (pg.K_a, pg.K_LEFT) and not game.game_over:
                 game.move_left()
-            if event.key == pg.K_RIGHT and not game.game_over:
+            if event.key in (pg.K_RIGHT,pg.K_d) and not game.game_over:
                 game.move_right()
-            if event.key == pg.K_DOWN and not game.game_over:
+            if event.key in (pg.K_DOWN,pg.K_s) and not game.game_over:
                 game.move_down()
-            if event.key == pg.K_UP and not game.game_over:
+            if event.key in (pg.K_UP,pg.K_w) and not game.game_over:
                 game.rotate()
         if event.type == GAME_UPDATE and not game.game_over:
             game.move_down()
-    screen.fill((44,44,127))
-    game.draw(screen)
+
+        if event.type == GAME_OVER:
+            t_b_s = next(text_blink_surf)
     if game.game_over:
-        screen.blit(text_surf,text_rect)
+        #screen.blit(stroke_surf,stroke_rect)
+        screen.blit(t_b_s,text_rect)
     # game.move_down()
     # rect = pg.Rect(100,100,100,400)
     # pg.draw.rect(screen,(47, 230, 23),rect)
